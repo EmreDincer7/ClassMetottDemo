b2e376761e671b75b176ee888be96b01d531edc4
emre dinçer <emre_dincer70@windowslive.com>
2021-02-10 21:59:54 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Başarı ile " + musteri.Name + " " + musteri.Surname + " adlı kaydınızı oluşturdunuz");
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri " + musteri.Name + " " + musteri.Surname + "silme işlemi başarı ile sonuçlandı");
+        }
+
+        public void List(Musteri musteri)
+        {
+            Console.WriteLine("Müşterinin adı :" + musteri.Name + "\n" + "Müşterinin soyadı :" + musteri.Surname + "\n" + "Müşterinin TC'si :" + musteri.IdenNumber);
+        }
+
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+
+            musteri1.ID = 1;
+            musteri1.Name = "Emre";
+            musteri1.Surname = "Dinçer";
+            musteri1.IdenNumber = 123456789;
+
+            Musteri musteri2 = new Musteri();
+
+            musteri2.ID = 2;
+            musteri2.Name = "Kerim";
+            musteri2.Surname = "Dinçer";
+            musteri2.IdenNumber = 987654321;
+
+            Musteri musteri3 = new Musteri();
+
+            musteri3.ID = 3;
+            musteri3.Name = "Meryem";
+            musteri3.Surname = "Dinçer";
+            musteri3.IdenNumber = 1122334455;
+
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            foreach ( Musteri item in musteriler)
+            {
+                Console.WriteLine("Müşterinin ismi : " + item.Name);
+                Console.WriteLine("Müşterinin Soy ismi : " + item.Surname);
+                Console.WriteLine("Müşterinin TC : " + item.IdenNumber);
+
+
+            }
+
+            Console.WriteLine("                      ");
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+            musteriManager.Add(musteri3);
+
+
+            Console.WriteLine("                      ");
+
+
+            musteriManager.Delete(musteri1);
+            musteriManager.Delete(musteri2);
+            musteriManager.Delete(musteri3);
+
+
+
+            Console.WriteLine("                      ");
+
+
+
+            musteriManager.List(musteri1);
+            musteriManager.List(musteri2);
+            musteriManager.List(musteri3);
+
+        }
+    }
+}
